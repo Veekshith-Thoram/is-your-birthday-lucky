1e982b219c1b1dd5fe2be9b0b7117de89a10a511
Veekshith-Thoram <veekshiththoram@gmail.com>
2021-08-14 19:28:32 +0530

    functions:calculate sum

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkButton = document.querySelector("#check-button");
 
-function clickHandler(){
-    
+function checkBirthDateIsLucky(){
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    console.log(sum);
 }
 
-checkButton.addEventListener("click", clickHandler);
+function calculateSum(dob){
+    dob = dob.replaceAll("-","");
+    let sum = 0;
+    for(let i=0;i<dob.length;i++){
+        sum = sum+Number(dob.charAt(i))
+    }
+    return sum;
+}
+
+checkButton.addEventListener("click", checkBirthDateIsLucky);
